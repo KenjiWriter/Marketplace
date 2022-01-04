7ae93bb3e4ec281dc576d80fd30605034a887be0
Maciej Wiśniewski <m.wisniewski@elpromaelectronics.com>
2022-01-04 22:35:16 +0100

    Fix: sending message to the seller

--- app/Http/Livewire/MessageSeller.php
+++ app/Http/Livewire/MessageSeller.php
@@ -20,25 +20,23 @@ class MessageSeller extends Component
             session()->flash('message', "You can't message to your self.");
         } else {
             $messages = message::where('sender', auth()->user()->id)->first();
-            if(isset($messages)) {
+            if($messages == NULL) {
                 $roomId = $messages["roomId"];
             } else {
-                $count = message::count();
-                $roomId = $count + 1;
-                $buyer = user::find(auth()->user()->id);
-                $accessible_rooms_buyer = json_decode($buyer->accessible_rooms, true);
-                $accessible_rooms_buyer[] = ['roomId' => $roomId];
-                $buyer->accessible_rooms = json_encode($accessible_rooms_buyer);
-
-                $seller = user::find($this->product_seller);
-                $accessible_rooms_seller = json_decode($seller->accessible_rooms, true);
-                $accessible_rooms_seller[] = ['roomId' => $roomId];
-                $seller->accessible_rooms = json_encode($accessible_rooms_seller);
+                $count   = message::count();
+                $roomId  = $count + 1;
             }
+            $buyer   = user::where('id', auth()->user()->id)->select('accessible_rooms')->first();
+            $accessible_rooms_buyer      = json_decode($buyer->accessible_rooms, true);
+            $accessible_rooms_buyer[]    = ['roomId' => $roomId];
+
+            $seller = user::where('id', $this->product_seller)->select('accessible_rooms')->first();
+            $accessible_rooms_seller     = json_decode($seller->accessible_rooms, true);
+            $accessible_rooms_seller[]   = ['roomId' => $roomId];
             $send_message = message::create(['roomId' => $roomId, 'product_id' => $this->product_id, 'sender' => auth()->user()->id, 'receiver' => $this->product_seller,'buyer' => auth()->user()->id, 'seller' => $this->product_seller, 'message' => $this->body]);
             if($send_message) {
-                $buyer->save();
-                $seller->save();
+                $buyer->update(['accessible_rooms' => json_encode($accessible_rooms_buyer)]);
+                $seller->update(['accessible_rooms' => json_encode($accessible_rooms_seller)]);
                 session()->flash('message', 'Message successfully sended!');
             } else {
                 session()->flash('message', 'Something went wrong! try again later.');
