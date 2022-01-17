e74adb33ac3d6fc1837a03ae11fa9afbd24e25da
Maciej Wiśniewski <m.wisniewski@elpromaelectronics.com>
2022-01-17 22:44:32 +0100

    Added new UI for adding new announcement

--- resources/views/livewire/add-post.blade.php
+++ resources/views/livewire/add-post.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <fieldset>
+    {{-- <fieldset>
         @if (session()->has('message'))
         <div class="alert alert-success">
             {{ session('message') }}
@@ -8,7 +8,7 @@
         <legend>Add new announcement</legend>
         <input type="text" wire:model="name" placeholder="name"><br>
         <label for="owner">I'm a first owner: </label> <input id="Owner" type="checkbox" wire:model="first_owner"><br>
-        Category: 
+        Category:  
         <select wire:model="category"> <br>
             <option value="0">Select category</option>
             <option value="1">Phones</option>
@@ -23,5 +23,52 @@
         <div wire:loading wire:target="photo">Uploading...</div>
         <br><br>
         <button wire:click.prevent="add">Add announcement</button>
-    </fieldset>
+    </fieldset> --}}
+    <div class="container border border-dark">
+        <div class="col-lg-4 col-lg-offset-4 form-row">
+            <h3>Add new announcement</h3>
+            @if (session()->has('message'))
+            <div class="alert alert-success">
+                {{ session('message') }}
+            </div>
+            @endif
+            @if (session()->has('error'))
+                <div class="alert alert-danger">
+                    {{ session('error') }}
+                </div>
+            @endif
+            <div class="form-group">
+                <label for="name">Title:</label>
+                <input class="form-control" type="text" wire:model="name" id="name" placeholder="Title">
+            </div>
+            <div class="form-group">
+                <label for="desc">Description: </label>
+                <textarea class="form-control" name="" id="desc" cols="30" rows="5"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="Owner">I'm a first owner: </label> <input id="Owner" class="checkbox" type="checkbox" wire:model="first_owner">
+            </div>
+            <div class="form-group">
+                <label for="category">Category:</label>
+                <select class="form-control" wire:model="category">
+                    <option value="0">Select category</option>
+                    <option value="1">Phones</option>
+                    <option value="2">Tablets</option>
+                    <option value="3">Computers</option>
+                    <option value="4">Other</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="price">Price: </label>
+                <input type="number" id="price" class="form-control" wire:model="price" placeholder="Price">
+            </div>
+            <div class="form-group">
+                <label for="file">Upload photos: </label>
+                <input type="file" class="form-control" id="file" wire:model="photos" multiple>
+                @error('photo') <span style="color: red;">{{ $message }}</span> @enderror
+                <div wire:loading wire:target="photo">Uploading...</div>
+            </div>
+            <button wire:click.prevent="add" class="form-control btn btn-success">Add announcement</button>
+        </div>
+    </div>
 </div>
